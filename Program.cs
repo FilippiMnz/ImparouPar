3735e7e56b91572f7f1717dceb9e94019f9aaa15
FilippiMnz <imperilkill12@gmail.com>
2022-07-12 17:24:52 -0300

    Kamehameha

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ParouImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aplicacao();
+        }
+        static void Aplicacao()
+        {
+            Console.WriteLine("Insira o numero para o teste: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            int teste = numero % 2;
+            if (teste == 0)
+            {
+                Console.WriteLine("O numero é par");
+            }
+            else
+            {
+                Console.WriteLine("O numero é impar");
+            };
+        }
+
+    }
+
+}
